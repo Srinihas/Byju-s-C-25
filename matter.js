627c88069c6f6c28499352559cc6691176850c41
Srinihas <115796290+Srinihas@users.noreply.github.com>
2022-12-06 20:32:17 +0400

    Update matter.js

--- matter.js
+++ matter.js
@@ -10359,3 +10359,5 @@ var Vector = _dereq_('../geometry/Vector');
 
 },{"../body/Composite":2,"../core/Common":14,"../core/Events":16,"../geometry/Bounds":26,"../geometry/Vector":28}]},{},[30])(30)
 });
+
+//Made by Srinihas. *DO NOT COPY  OR ElSE ME GONNA REPORT*
